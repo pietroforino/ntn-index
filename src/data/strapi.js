b4b1b7c264dadcc240edc8efe3b1a202fde7cde1
pietroforino <pietrof@live.com>
2025-05-12 00:06:51 +0200

    update lightbox logic

---
+++ src/data/strapi.js
@@ -0,0 +1,20 @@
+export async function getProjects() {
+  try {
+    // const response = await fetch('https://cmsportfolio-production.up.railway.app/api/projects?populate=*');
+    const response = await fetch('http://localhost:1337/api/projects?customPopulate=nested');
+    
+    
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    console.log("Dati ricevuti da Strapi:", data);
+
+    return data.data; // Assicurati che questa sia la struttura corretta
+  } catch (error) {
+    console.error('Errore durante il fetch dei progetti da Strapi:', error);
+    return [];
+  }
+}
