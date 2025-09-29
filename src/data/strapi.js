b96acd80d6e9edcc73f9178beedd6151cd71c43b
pietroforino <pietrof@live.com>
2025-09-29 22:29:10 +0200

    fix seo

--- src/data/strapi.js
+++ src/data/strapi.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -6,11 +6,12 @@ const STRAPI_MEDIA_BASE = process.env.STRAPI_URL;
 
 // const STRAPI_MEDIA_BASE = 'http://localhost:1337';
 
-
 export async function getProjects() {
   try {
-    const response = await fetch(STRAPI_MEDIA_BASE + '/api/projects?populate=*');
-    
+    const response = await fetch(
+      STRAPI_MEDIA_BASE + "/api/projects?populate=*"
+    );
+
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
@@ -21,15 +22,15 @@ export async function getProjects() {
 
     return data.data;
   } catch (error) {
-    console.error('Errore durante il fetch dei progetti da Strapi:', error);
+    console.error("Errore durante il fetch dei progetti da Strapi:", error);
     return [];
   }
 }
 
 export async function getInformations() {
   try {
-    const response = await fetch(STRAPI_MEDIA_BASE + '/api/about?populate=*');
-    
+    const response = await fetch(STRAPI_MEDIA_BASE + "/api/about?populate=*");
+
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
@@ -40,16 +41,15 @@ export async function getInformations() {
 
     return data.data;
   } catch (error) {
-    console.error('Errore durante il fetch dei progetti da Strapi:', error);
+    console.error("Errore durante il fetch dei progetti da Strapi:", error);
     return [];
   }
 }
 
-
 export async function getContacts() {
   try {
-    const response = await fetch(STRAPI_MEDIA_BASE + '/api/contact?populate=*');
-    
+    const response = await fetch(STRAPI_MEDIA_BASE + "/api/contact?populate=*");
+
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
@@ -60,7 +60,7 @@ export async function getContacts() {
 
     return data.data;
   } catch (error) {
-    console.error('Errore durante il fetch dei progetti da Strapi:', error);
+    console.error("Errore durante il fetch dei progetti da Strapi:", error);
     return [];
   }
 }
